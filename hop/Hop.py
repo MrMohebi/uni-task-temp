56b88d4f318074c244f23f9233d0b0e71a459164
mmmohebi <MMMohebi@outlook.com>
2023-04-02 22:14:13 +0430

    init

---
+++ hop/Hop.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+def is_integer(n):
+    try:
+        float(n)
+    except ValueError:
+        return False
+    else:
+        return float(n).is_integer()
+
+
+number = 1
+computerTurn = False
+while True:
+    if computerTurn:
+        result = str(number)
+        if number % 5 == 0:
+            result = str('hope :)')
+        print("my number: " + result)
+
+    else:
+        userNumber = input('ur turn: ')
+        if (number % 5 == 0 and userNumber != 'hop') or (is_integer(userNumber) and number != int(userNumber)):
+            print('wrong number, try again')
+            exit()
+            
+            continue
+
+    number += 1
+    computerTurn = not computerTurn
+
+
+# In[ ]:
+
+
+
+
