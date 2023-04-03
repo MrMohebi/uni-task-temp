9ac7529f451135aca27a7d9ec9340f400041a4fd
MMMohebi <MMMohebi@outlook.com>
2023-04-03 10:30:07 +0330

    thread dic

--- dictionary/serverDic.py
+++ dictionary/serverDic.py
@@ -1,5 +1,6 @@
 import random
 import socket
+import threading
 
 words = {
     "a": ["abandon", "abroad"],
@@ -7,22 +8,13 @@ words = {
     "c": ["ceb"]
 }
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server_address = ('localhost', 8000)
-server_socket.bind(server_address)
-
-server_socket.listen(1)
-print('Server is listening on {}:{}'.format(*server_address))
-
-while True:
-    client_socket, client_address = server_socket.accept()
+def handle_client(client_socket, client_address):
     print('Received connection from', client_address)
 
-    # Receive data from the client
     data = client_socket.recv(1024).decode()
     if not data:
-        break
+        return
 
     user = data
     last_char = user[-1]
@@ -36,3 +28,18 @@ while True:
 
     client_socket.close()
     print('Closed connection with', client_address)
+
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server_address = ('localhost', 8000)
+server_socket.bind(server_address)
+
+server_socket.listen(1)
+print('Server is listening on {}:{}'.format(*server_address))
+
+while True:
+    client_socket, client_address = server_socket.accept()
+
+    t = threading.Thread(target=handle_client, args=(client_socket, client_address))
+    t.start()
